9f8f015f9bfafa23c6a63e931262e627cd31c507
Gustavo12345-12345 <85851468+Gustavo12345-12345@users.noreply.github.com>
2025-06-28 17:21:22 -0300

    db.js

--- db.js
+++ db.js
@@ -1,24 +1,28 @@
+
+
 // db.js
-const Database = require('better-sqlite3');
+const sqlite3 = require('sqlite3').verbose();
 const path = require('path');
 
-const db = new Database(path.join(__dirname, 'database.db'));
+const dbPath = path.join(__dirname, 'database.db');
+const db = new sqlite3.Database(dbPath);
 
-// Cria a tabela se não existir
-db.prepare(`
-  CREATE TABLE IF NOT EXISTS registros (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    Projeto TEXT,
-    TipoObra TEXT,
-    TipoProjeto TEXT,
-    TipoDoc TEXT,
-    Disciplina TEXT,
-    Sequencia TEXT,
-    Revisao TEXT,
-    CodigoArquivo TEXT UNIQUE,
-    Data TEXT,
-    Autor TEXT
-  )
-`).run();
+db.serialize(() => {
+  db.run(`
+    CREATE TABLE IF NOT EXISTS registros (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      Projeto TEXT,
+      TipoObra TEXT,
+      TipoProjeto TEXT,
+      TipoDoc TEXT,
+      Disciplina TEXT,
+      Sequencia TEXT,
+      Revisao TEXT,
+      CodigoArquivo TEXT UNIQUE,
+      Data TEXT,
+      Autor TEXT
+    )
+  `);
+});
 
 module.exports = db;
